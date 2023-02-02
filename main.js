bfa68e3ec7a434e492f631664741bcb04e4fa04a
LGzzus <jesussosa9859@gmail.com>
2023-02-02 09:24:18 -0600

    Se crearon las funciones de encryptar

--- main.js
+++ main.js
@@ -1,27 +1,39 @@
-function check(){
-    let char1 = document.getElementById("char1").value;
-    let char2 = document.getElementById("char2").value;
-    let char3 = document.getElementById("char3").value;
-    let char4 = document.getElementById("char4").value;
-    let char5 = document.getElementById("char5").value;
-    let charv1 = document.getElementById("charv1").value;
-    let charv2 = document.getElementById("charv2").value;
-    let charv3 = document.getElementById("charv3").value;
-    let charv4 = document.getElementById("charv4").value;
-    let charv5 = document.getElementById("charv5").value;
-    
-    let concat = char1+char2+char3+char4+char5;
-
-    let concat2 = charv1+charv2+charv3+charv4+charv5;
-    console.log(concat);
-    console.log(concat2);
-
-    if(concat == concat2){
-        console.log("Son iguales")
-        document.append(alert("Contraseñas verificadas"))
-    }else{
-    console.log("son iniguales")
-    document.append(alert("verificar contraseñas"))
-
-   } 
+function save(){
+    var userC = "";
+    var userC1 = new Array();
+    var userC2 = new Array();
+    var psw1 = document.getElementsByClassName('psw1'),
+    claveValues = [].map.call(psw1,function(dataInput){
+        userC1.push(dataInput.value);
+    });
+    var psw2 = document.getElementsByClassName('psw2'),
+    claveValues = [].map.call(psw2,function(dataInput){
+        userC2.push(dataInput.value);
+    });
+    userC1.forEach(function(inputsValuesData){
+        console.log(inputsValuesData);
+         userC = userC + inputsValuesData;
+         console.log(userC.toString())
+     });
+    userC2.forEach(function(inputsValuesData){
+       console.log(inputsValuesData);
+        userC = userC + inputsValuesData;
+        console.log(userC.toString())
+    });
+}
+    //Esta funcion tambien es valida para iterar a los elementos del array
+    /*var userC="";
+    userA.forEach(element =>{
+        console.log(element);
+        userC = userC +  element;
+        console.log('---asdsd');
+        console.log(userC);
+    });*/
+function encrypt(){
+    var user = document.getElementById("user").value; //get value from register.html
+    var pass = "TN";//Password for run encrypt
+    var cif = CryptoJS.AES.encrypt(user,pass);// method encrypt
+    var des = CryptoJS.AES.decrypt(cif,pass);// decrypt method to check 
+    console.log(cif.toString());
+    console.log(des.toString(CryptoJS.enc.Utf8));
 }
